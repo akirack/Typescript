51e6d23d5626120e65f1abfc071e3b5c73e09bf1
akirac <couzack_2009@hotmail.com>
2022-10-28 23:09:18 -0300

    add feat/v1

--- app.ts
+++ app.ts
@@ -1,9 +1,47 @@
 class Validator{
     data : string | number | boolean | undefined;
+
+    constructor(data : any) {
+        this.data = data;
+    }
+
 }
 
-const validator = new Validator();
-validator.data = "joao"
+class StringValidator extends Validator{
+    data : string;
 
-const validator1 = new Validator();
-validator.data = 10;
+    constructor(data : string) {
+        try {
+            super(data);
+            this.data = data;
+        } catch {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+
+class NumberValidator extends Validator{
+    data : number;
+
+    constructor(data : number) {
+        try {
+            super(data);
+            this.data = data;
+        } catch {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+
+class BoolenValidator extends Validator{
+    data : boolean;
+
+    constructor(data : boolean) {
+        try {
+            super(data);
+            this.data = data;
+        } catch {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
