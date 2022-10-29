fea0a1316fa92669a80704293d63a17f80838880
akirac <couzack_2009@hotmail.com>
2022-10-28 22:58:32 -0300

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+var Validator = /** @class */ (function () {
+    function Validator() {
+    }
+    return Validator;
+}());
+
+var validator = new Validator();
+validator.data = "joao";
+var validator1 = new Validator();
+validator.data = 10;
---
+++ app.ts
@@ -0,0 +1,9 @@
+class Validator{
+    data : string | number | boolean | undefined;
+}
+
+const validator = new Validator();
+validator.data = "joao"
+
+const validator1 = new Validator();
+validator.data = 10;
